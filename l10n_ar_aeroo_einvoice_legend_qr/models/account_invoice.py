be29a0fd50ea778a764bfe0ddf85131e8388459a
paradisocrist <crisparadiso_@hotmail.com>
2021-05-05 17:07:55 -0300

    [FIX][l10n_ar_aeroo_einvoice_legend_qr]ODOO-1233 now legend_in_document field atocompletes by a compute function

--- l10n_ar_aeroo_einvoice_legend_qr/models/account_invoice.py
+++ l10n_ar_aeroo_einvoice_legend_qr/models/account_invoice.py
@@ -3,12 +3,15 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    legend_in_document = fields.Text(string = "Legend in Document")
+    legend_in_document = fields.Text(string = "Legend in Document", compute="compute_journal_document_type_id")
+    
+    @api.multi
+    @api.depends('journal_document_type_id','company_id')
+    def compute_journal_document_type_id(self):
+        if self.company_id and self.journal_document_type_id:
 
-    @api.onchange('journal_document_type_id')
-    def onchange_journal_document_type_id(self):
-        
-        legend_id = self.env['default.legend.document.company'].search([('company', '=', self.company_id.id),
-                                                                        ('document_type', '=', self.document_type_id.id)])
-        if legend_id:
-            self.legend_in_document = legend_id.default_legend
+            legend_id = self.env['default.legend.document.company'].search([('company', '=', self.company_id.id),
+                                                                            ('document_type', '=', self.document_type_id.id)])
+            if legend_id:
+                if self.legend_in_document == "Add a comment to your report..." or not self.legend_in_document:
+                    self.legend_in_document = legend_id.default_legend
